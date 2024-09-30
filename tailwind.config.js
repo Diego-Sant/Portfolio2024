79428ce45759a10845d196adf8e45cfc67f44798
Diego de Souza Santana <diegossantana068@gmail.com>
2024-09-30 17:58:14 -0300

    tailwind config + font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,41 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+	container: {
+		center: true,
+		padding: "15px",
+	},
+	screens: {
+		sm: '640px',
+		md: '768px',
+		lg: '960px',
+		xl: '1200px',
+	},
+	fontFamily: {
+		primary: "var(--font-jetbrainsMono)",
+	},
   	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
+		colors: {
+			primary: '#121212',
+			accent: {
+				DEFAULT: '#00ff99',
+				hover: '#00e187'
+			}
+		},
+  		keyframes: {
+			"accordion-down": {
+				from: {height: "0"},
+				to: { height: "var(--radix-accordion-content-height)"},
+			},
+			"accordion-up": {
+				from: {height: "var(--radix-accordion-content-height)"},
+				to: {height: "0"},
+			},
+		},
+		animation: {
+			"accordion-down": "accordion-down 0.2s ease-out",
+			"accordion-up": "accordion-up 0.2s ease-out"
+		},
   	}
   },
   plugins: [require("tailwindcss-animate")],
